18182177bbfe2890b96f15095608d14b7c471802
Chance Smith <chance@sodiumhalogen.com>
2016-04-09 22:10:03 -0500

    init commit - retarting repo

---
+++ gigs.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Latest Gigs | Chance Smith</title>
+
+    <!-- Bootstrap -->
+    <link href="_/css/bootstrap.css" rel="stylesheet">
+    <link href="_/css/styles.css" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <?php include ('_/components/php/google-analytics.php'); ?>
+  </head>
+  <body>
+    <?php include ('_/components/php/nav.php'); ?>
+
+    <div class="container sideProject">
+
+      <!-- Main component for a tools -->
+      <div class="jumbotron latest-work">
+        <h1>Latest Gigs</h1>
+      </div>
+      <h2>2016</h2>
+      <div class="jumbotron pms">
+        <h2><i class="fa fa-square-o"></i> Foundation Bank</h2>
+      </div>
+      <div class="jumbotron pms">
+        <h2><i class="fa fa-square-o"></i> Leaders Credit Union</h2>
+      </div>
+      <div class="jumbotron pms">
+        <h2><i class="fa fa-square-o"></i> Kontakt Mission USA</h2>
+      </div>
+      <div class="jumbotron pms bucketListDone">
+        <h2><i class="fa fa-check-square-o"></i> Iron.io (2.0) <a class="btn btn-md btn-default" href="http://iron.io" role="button">Site &raquo;</a></h2>
+      </div>
+      <div class="jumbotron pms bucketListDone">
+        <h2><i class="fa fa-check-square-o"></i> 22 Laws of Marketing <a class="btn btn-md btn-default" href="http://22lawsofmarketing.com" role="button">Site &raquo;</a></h2>
+      </div>
+      <div class="jumbotron pms bucketListDone">
+        <h2><i class="fa fa-check-square-o"></i> Butts Foods <a class="btn btn-md btn-default" href="http://buttsfoods.com" role="button">Site &raquo;</a></h2>
+      </div>
+      <h2>2015</h2>
+      <div class="jumbotron pms bucketListDone">
+        <h2><i class="fa fa-check-square-o"></i> Find A Pathway <a class="btn btn-md btn-default" href="http://findapathway.com" role="button">Site &raquo;</a></h2>
+      </div>
+      <div class="jumbotron pms bucketListDone">
+        <h2><i class="fa fa-check-square-o"></i> PrimaryIO <a class="btn btn-md btn-default" href="http://primaryio.com" role="button">Site &raquo;</a></h2>
+      </div>
+      <div class="jumbotron pms bucketListDone">
+        <h2><i class="fa fa-check-square-o"></i> Allygn <a class="btn btn-md btn-default" href="http://allygn.com" role="button">Site &raquo;</a></h2>
+      </div>
+      <div class="jumbotron pms bucketListDone">
+        <h2><i class="fa fa-check-square-o"></i> Mystoree <a class="btn btn-md btn-default" href="http://mystor.ee" role="button">Site &raquo;</a></h2>
+      </div>
+      <div class="jumbotron pms bucketListDone">
+        <h2><i class="fa fa-check-square-o"></i> Aldersgate UMC <a class="btn btn-md btn-default" href="http://jacksonfumc.org" role="button">Site &raquo;</a></h2>
+      </div>
+      <div class="jumbotron pms bucketListDone">
+        <h2><i class="fa fa-check-square-o"></i> FUMC <a class="btn btn-md btn-default" href="http://jacksonfumc.org" role="button">Site &raquo;</a></h2>
+      </div>
+      <div class="jumbotron pms bucketListDone">
+        <h2><i class="fa fa-check-square-o"></i> Iron.io (1.0) <a class="btn btn-md btn-default" href="http://iron.io" role="button">Site &raquo;</a></h2>
+      </div>
+      <div class="jumbotron pms bucketListDone">
+        <h2><i class="fa fa-check-square-o"></i> Jane Ross Tutoring <a class="btn btn-md btn-default" href="https://janerosstutoring.com" role="button">Site &raquo;</a></h2>
+      </div>
+      <div class="jumbotron pms bucketListDone">
+        <h2><i class="fa fa-check-square-o"></i> Tulum <a class="btn btn-md btn-default" href="http://www.tulumfreshmex.com/" role="button">Site &raquo;</a></h2>
+      </div>
+      <div class="jumbotron pms bucketListDone">
+        <h2><i class="fa fa-check-square-o"></i> Renew BioMedical <a class="btn btn-md btn-default" href="http://www.renewbiomedical.com/" role="button">Site &raquo;</a></h2>
+      </div>
+      <div class="jumbotron pms bucketListDone">
+        <h2><i class="fa fa-check-square-o"></i> Dream Girl Fishing Boat <a class="btn btn-md btn-default" href="http://dreamgirlsportfishingcr.com" role="button">Site &raquo;</a></h2>
+      </div>
+      <h2>2014</h2>
+      <div class="jumbotron pms bucketListDone">
+        <h2><i class="fa fa-check-square-o"></i> TIPS prints <a class="btn btn-md btn-default" href="http://tipsprints.com" role="button">Site &raquo;</a></h2>
+      </div>
+      <h2>2013</h2>
+      <div class="jumbotron pms bucketListDone">
+        <h2><i class="fa fa-check-square-o"></i> Pledge To Health <a class="btn btn-md btn-default" href="http://www.pledgetohealth.com.php56-9.dfw3-1.websitetestlink.com/" role="button">Site &raquo;</a></h2>
+      </div>
+
+    </div> <!-- /container -->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="_/js/bootstrap-ck.js"></script>
+    <script src="_/js/myscript-ck.js"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Test Every Idea | ChanceSmith.org</title>
+
+    <!-- Bootstrap -->
+    <link href="_/css/bootstrap.css" rel="stylesheet">
+    <link href="_/css/styles.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <?php include ('_/components/php/google-analytics.php'); ?>
+</head>
+
+<body>
+    <?php include ('_/components/php/nav.php'); ?>
+
+        <div class="container project">
+
+            <!-- Main component for a primary marketing message or call to action -->
+            <div class="jumbotron chancesmith">
+                <h1 class="visible-lg">Chance Smith</h1>
+                <h1 class="hidden-lg">I'm Chance</h1>
+                <h2 class="visible-lg">Strategist | Leader | Developer</h2>
+                <p class="visible-lg">Here are the businesses and projects I'm excitedly apart of.</p>
+            </div>
+
+            <div class="jumbotron blog" id="blog">
+                <h1>Daily Strategy</h1>
+                <p>In the world of strategy there is more learning then there is success.</p>
+                <p>
+                    <a class="btn btn-lg btn-primary" href="blog.chancesmith.org" role="button">View the blog &raquo;</a>
+                </p>
+            </div>
+
+            <div class="jumbotron workshop">
+                <h1>Onboarding Workshop</h1>
+                <p>Do you need to grow your team fast? Do you have high turnover?</p>
+                <p>I'll save you money by helping you build a process to onboard the right hires.</p>
+                <p>
+                    <a class="btn btn-lg btn-primary" href="https://chance3.typeform.com/to/xYvHeP" role="button">Apply for Workshop &raquo;</a>
+                </p>
+            </div>
+
+            <div class="jumbotron latest-work">
+                <h1>Latest Work</h1>
+                <p>Here's what I've been up to.</p>
+                <p>
+                    <a class="btn btn-lg btn-default" href="/gigs.php" role="button">Latest gigs &raquo;</a>
+                </p>
+            </div>
+
+            <div class="jumbotron otherProjects">
+                <h1>Side Projects</h1>
+                <p>There are always other irons in the fire. I have some side items that I've put up here. I hope by adding these projects someone will find a use for them or get inspired.</p>
+                <p>
+                    <a class="btn btn-lg btn-primary" href="/projects.php" role="button">View other projects &raquo;</a>
+                </p>
+            </div>
+
+        </div>
+        <!-- /container -->
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="_/js/bootstrap-ck.js"></script>
+        <script src="_/js/myscript-ck.js"></script>
+</body>
+
+</html>
