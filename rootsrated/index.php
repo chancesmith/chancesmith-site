d0ab9858cb4b37144c586643659bfb854d64d60d
Chance Smith <chancesmithb@gmail.com>
2016-08-25 11:41:03 -0500

    adds rootsrated for stackoverflow submission

---
+++ rootsrated/index.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>RootsRated | ChanceSmith.org</title>
+
+    <!-- >Bootstrap -->
+    <link href="../../_/css/bootstrap.css" rel="stylesheet">
+    <link href="../../_/css/styles.css" rel="stylesheet">
+
+    <!-- >HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- >WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <?php include ('../../_/components/php/google-analytics.php'); ?>
+		
+		<link href="https://fonts.googleapis.com/css?family=Bitter" rel="stylesheet">
+    <style>
+    	body {
+    		background-color: #ccc;
+    		background: linear-gradient( 135deg, rgba(38, 38, 38, 0), rgba(53, 53, 53, 0.75), rgba(51, 51, 51, 0.96) ), url('somewhere-awesome.jpg') center center fixed no-repeat;
+    		/*background: url(somewhere-awesome.jpg) no-repeat center center fixed; */
+			  -webkit-background-size: cover;
+			  -moz-background-size: cover;
+			  -o-background-size: cover;
+			  background-size: cover;
+    	}
+    	.rootsRated img.logo {
+    		padding-bottom: 30px;
+    	}
+    	h1, h2 {
+    		font-family: 'Bitter', serif;
+    		color: white;
+    	}
+    	h1 {
+    		padding-bottom: 45px;
+    	}
+    	h2 {
+    		text-align: left;
+    	}
+    	a {
+    		font-weight: bold;
+    	}
+    	h2.reason {
+				width: 60%;
+				margin: 80px auto;
+				line-height: 1.7em;
+			}
+			@media (max-width: 1000px) {
+				h2.reason {
+					width: 80%;
+				}
+			}
+			@media (max-width: 760px) {
+				h2.reason {
+					width: 100%;
+				}
+			}
+    	.projects {
+    		margin: 67px 0 0 0;
+    	}
+    	.projects .wrapper {
+    		width: 51%;
+    		margin: 0 auto;
+    	}
+    	@media (max-width: 1000px) {
+				.projects .wrapper {
+					width: 80%;
+				}
+			}
+			@media (max-width: 760px) {
+				.projects .wrapper {
+					width: 100%;
+				}
+			}
+    	.projects ul {
+		    background: rgba(255, 255, 255, 0.80);
+				padding: 27px 32px 30px;
+			}
+    	.projects ul li {
+		    list-style: none;
+				text-align: left;
+				font-size: 17px;
+				color: #272727;
+		    padding: 0 0 24px 0;
+			}
+			.projects ul li p, .projects, ul p {
+				text-align: left;
+			}
+			.projects ul li:last-of-type {
+		    padding: 0;
+			}
+			.projects ul li a {
+				color: #428bca;
+				text-decoration: none;
+				font-size: 22px;
+				font-weight: bold;
+			}
+			.projects ul li span {
+				color: #4a4a4a;
+				text-decoration: none;
+				font-size: 22px;
+				font-weight: bold;
+			}
+			footer {
+				text-align: center;
+				color: white;
+				padding: 45px 0;
+			}
+			footer p {
+				margin: 54px 0;
+		    font-size: 25px;
+			}
+			footer span {
+				font-weight: bold;
+			}
+    </style>
+</head>
+
+<body>
+  <div class="container rootsRated text-center">
+  	<a href="https://rootsrated.com"><img src="rootsrated-logo.png" alt="RootsRated logo" class="logo"></a>
+		<h1>RootsRated, this hive-five is for you!</h1>
+		<img src="../gifs/high-five-me.gif" alt="Chance wants to give you a high five RootsRated">
+		
+		<h2 class="text-center reason">This was created to share more information requested in your job posting on <a href="http://stackoverflow.com/jobs/122818/frontend-engineer-for-an-outdoors-media-startup-rootsrated">Stackoverflow</a></h2>
+
+		<div class="projects">
+			<div class="wrapper">
+				<h2>Side projects:</h2>
+				<ul>
+					<li><a href="formedmeteor.herokuapp.com/">FormedApp</a> - <a href="formedmeteor.herokuapp.com/dashboard">[test dashboard]</a><br />Started in April, FormedApp is a platform under development for youth groups to engage with scripture together (MeteorJS)</li>
+					<li><span>Prism</span> <br />App helping land owners know when to harvest their trees (iOS)</li>
+					<li><a href="https://github.com/attheCO/trophy-countdown-rtc-chip">theCO Trophy countdown</a><br />a trophy that counts down to the next Dev.Catalyst awards event (arduino)</li>
+					<li><a href="https://betterjoblanding.com">Better Job Landing</a> <br />A paperless way for Smoothie King Jackson, TN to accept applications and qualify new candidates (Wordpress)</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="projects">
+			<div class="wrapper">
+				<h2>Things you'd love to see:</h2>
+				<ul>
+					<li><a href="https://foundationbank.org/">Foundation Bank</a> <br />Launched in April 2016 (Wordpress)</li>
+					<p>Try the decision tree (javascript) on the header of the homepage. Each choice leads to another choice or a page that matches your selection.</p>
+				</ul>
+			</div>
+		</div>
+
+		<div class="projects">
+			<div class="wrapper">
+				<h2>History of current healthy relationships:</h2>
+				<ul>
+					<li><span>William Donnell</span> <br />3 great years of working with the founder of Sodium Halogen</li>
+					<li><span>Adam Curl</span> <br />new intern/Jr developer at Sodium Halogen this year</li>
+					<li><span>Ben Harris</span> <br />2 years of working near COO of PopVox</li>
+					<li><span>Matt Ingram</span> <br />10 awesome years of working for the owner of Smoothie King Jackson, TN</li>
+					<li><span>Jeremy Akins</span> <br />2 years of helping General Manager of Smoothie King Jackson, TN</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="projects">
+			<div class="wrapper">
+				<h2>Work projects:</h2>
+				<ul>
+					<li><a href="http://gokmusa.org/">KontaktMission</a> <br />just launched in July 2016 (Wordpress)</li>
+					<li><a href="https://iron.io">Iron.io</a> <br />launched in February 2016 (Wordpress)</li>
+					<li><a href="http://app.mystor.ee/">Mystoree</a> <br />launched in December 2015 (Node.js/MeteorJS)</li>
+				</ul>
+			</div>
+		</div>
+
+		<footer>
+			<p>Chance gives <span>:nerdy-wave:</span> to ya, <a href="https://rootsrated.com">RootsRated</a>.</p>
+
+			<img src="nerdy-hello.gif" alt="super nerdy hello from Chance">
+
+			<p>Tweet me <a href="https://twitter.com/chance_smith">@chance_smith</a></p>
+		</footer>
+
+  </div>
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="../../_/js/bootstrap-ck.js"></script>
+  <script src="../../_/js/myscript-ck.js"></script>
+</body>
+
+</html>
+
+<?php 
+/**
+ 
+RootsRated,
+
+I can offer you seven years of experience building dynamic and static
+websites/web-apps for tech startup companies. I have excellent project management
+skills, and an exceptional eye for detail, all of which should make me the
+ideal candidate for this opening.
+
+I've spent three years full-time building with HTML/JS/PHP. Depending on the project, last couple years has been building with Wordpress or MeteorJS. I've just recently begun building an app with RoR, but I'm very much a novice that's ready to learn more. 
+
+Besides giving awesome high-fives and fist-bumps, I also dabble with the Raspberry Pi to build metric dashboards and scan networks.
+
+I'm a big fan of the scientific method, but only while bathing in data. I enjoy devising and building A/B tests to improve an experience that affects the bottom line.
+
+I have submitted my resume for your review and would welcome the chance
+to speak with you sometime.
+
+High-five? #gif  http://chancesmith.org/rootsrated/
+
+
+*/
+?>
